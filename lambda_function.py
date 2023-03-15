8a6a61ca867ccfe31ee6542a33278123c451fe5a
Sergei Epifanov <pifych@gmail.com>
2023-03-15 12:50:55 +0400

    small log level fixes

--- lambda_function.py
+++ lambda_function.py
@@ -9,30 +9,32 @@ from logging import info, debug
 
 def lambda_handler(event: dict, context) -> dict:
     _init_logging()
-    info('start')
+    debug('start')
     chat_id, result_text = _get_chat_id_and_text(event)
     tg.send_message(chat_id, result_text)
-    info('finish')
+    debug('finish')
     return {'statusCode': 200, 'body': 'Success'}
 
 
 def _get_chat_id_and_text(event: dict) -> tuple[int, str]:
     update_message = tg.get_update_message(event)
     info(f'{update_message = }')
-    if update_message and update_message.get('voice'):
-        chat_id = int(update_message.get('chat', {}).get('id', 0))
-        result_text = _get_text(update_message) if chat_id else ''
-        return chat_id, result_text
-    return 0, ''
+    if not update_message or not update_message.get('voice'):
+        return 0, ''        
+
+    chat_id = int(update_message.get('chat', {}).get('id', 0))
+    result_text = _get_text(update_message) if chat_id else ''
+    return chat_id, result_text
+
 
 
 def _get_text(message: dict) -> str:
-    info('start')
+    debug('start')
     voice_url = tg.get_voice_url(message)
     wav_in_memory = transcoder.transcode_opus_ogg_to_wav(voice_url)
     message_text = transcriptor.get_text(wav_in_memory)
     info(f'{message_text = }')
-    info('finish')
+    debug('finish')
     return message_text
 
 
@@ -43,7 +45,7 @@ def _init_logging() -> None:
             root_logger.removeHandler(handler)
     datefmt='%H:%M:%S'
     FORMAT = "[%(asctime)s %(filename)20s:%(lineno)5s - %(funcName)25s() ] %(message)s"
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format=FORMAT, 
                         datefmt=datefmt,
                         stream=sys.stdout)
