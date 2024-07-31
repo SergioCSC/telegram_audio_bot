e7a56029f00ba2479491ba02a2e6e02c42bddf6a
Sergey Epifanov <s.epifanov@icvr.io>
2024-08-01 01:17:07 +0400

    fix previous commit

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ IN_LINUX = sys.platform == 'linux'
 
 OPEN_AI_API_KEY = environ['OPEN_AI_API_KEY']
 TELEGRAM_BOT_TOKEN = environ['TELEGRAM_BOT_TOKEN']
+IN_AWS_LAMBDA = 'AWS_LAMBDA_RUNTIME_API' in environ
 AWS_LAMBDA_API_GATEWAY_URL = environ.get('AWS_LAMBDA_API_GATEWAY_URL')
 
 LOG_LEVEL = environ.get('LOG_LEVEL', 'INFO')
