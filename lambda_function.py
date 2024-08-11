5b9ef48e980fed40505292ee9e5256902d9c9b44
Sergey Epifanov <s.epifanov@icvr.io>
2024-08-12 02:11:00 +0400

    move checks from lambda_handler() to _get_text_and_chat_id()

--- lambda_function.py
+++ lambda_function.py
@@ -14,53 +14,19 @@ from logging import info, debug
 
 
 SUCCESSFULL_RESPONSE = {'statusCode': 200, 'body': 'Success'}
+EMPTY_RESPONSE_STR = 'EMPTY_RESPONSE_STR'
 
 
 def lambda_handler(event: dict, context) -> dict:
     _init_logging()
     debug('start')
     update_message: dict = tg.get_update_message(event)
-    
-    debug(f'{update_message = }')
-    if not update_message:
-        return SUCCESSFULL_RESPONSE
-    
-    chat_id = int(update_message.get('chat', {}).get('id', 0))
-    if not chat_id:
-        return SUCCESSFULL_RESPONSE
-    
-    if 'voice' in update_message \
-            or 'audio' in update_message \
-            or 'video' in update_message \
-            or 'video_note' in update_message \
-            or 'video' in update_message.get('document', {}).get('mime_type', ''):
-        
-        result_text = _get_text(update_message)
-    
-    elif 'text' in update_message:
-        input_text = update_message.get('text')
-        
-        if not input_text or input_text.lower() == '/start':
-            result_text = '''
 
-With me you can:
+    result_text, chat_id = _get_text_and_chat_id(update_message)
 
-  - Ask something from chatGPT: Just text me
-  - Transcribe an audio or a voice message: Send it to me
-  - Fix grammar in text in any language. Write "correct: my_text" or "правь: мой_текст"
-  - Translate text into English or Russian. Write: "translate: my_text"  or "переведи: мой текст"
+    if chat_id != -1:
+        tg.send_message(chat_id, result_text)
 
-The bot doesn't collect any info. Author: @n_log_n
-
-                    '''
-        else:
-            tg.send_message(chat_id, 'I have to think about it. Just a moment ...')
-            result_text = _get_text(update_message)
-    else:
-        result_text = "It seems I can't do what you want. \
-                I can answer to text and transcribe audio into text."
-    
-    tg.send_message(chat_id, result_text)
     debug('finish')
     return SUCCESSFULL_RESPONSE
 
@@ -107,12 +73,19 @@ def _sizeof_fmt(num:int, suffix: str = "B") -> str:
     return f"{num:.1f} Y{suffix}"
     
 
-def _get_text(message: dict, chat_temp: float = 1) -> str:
+def _get_text_and_chat_id(message: dict, chat_temp: float = 1) -> tuple[str, int]:
     debug('start')
+    debug(f'{message = }')
     
-    model = cfg.HUGGING_FACE_MODEL
-    chat_id = message.get('chat', {}).get('id', 0)
+    if not message:
+        debug(f'EMPTY_RESPONSE_STR')
+        return EMPTY_RESPONSE_STR, -1
     
+    chat_id = int(message.get('chat', {}).get('id', 0))
+    if not chat_id:
+        debug(f'EMPTY_RESPONSE_STR\n\n{chat_id = }\n\n{message = }')
+        return EMPTY_RESPONSE_STR, -1
+
     if message.get('audio') or message.get('voice') \
             or message.get('video') or message.get('video_note') \
             or 'video' in message.get('document', {}).get('mime_type', ''):
@@ -143,11 +116,14 @@ def _get_text(message: dict, chat_temp: float = 1) -> str:
             # mp3_bytes_io.name = 'my_audio_message.mp3'
             # output_text = openai_conn.audio2text(mp3_bytes_io, 'mp3')
             pass
+
+        model = cfg.HUGGING_FACE_MODEL
+
         tg.send_message(chat_id, f'{prefix}\nModel: {model} \
                         \n\nGetting media from Telegram ...')
         media_url = tg.get_media_url(message, chat_id)
         if not media_url:
-            return ''
+            return '', chat_id
         response = requests.get(media_url)
         if message.get('video') or message.get('video_note') \
                 or 'video' in message.get('document', {}).get('mime_type', ''):
@@ -204,16 +180,34 @@ def _get_text(message: dict, chat_temp: float = 1) -> str:
         output_text = f'{prefix}\nModel: {model}\n\nText: {output_text}'
 
     elif input_text := message.get('text'):
-        input_text = _correct_prompt(input_text)
-        output_text = openai_conn.chat(input_text, chat_temp)
+        if not input_text or input_text.lower() == '/start':
+            result_text = '''
+
+With me you can:
+
+  - Ask something from chatGPT: Just text me
+  - Transcribe an audio or a voice message: Send it to me
+  - Fix grammar in text in any language. Write "correct: my_text" or "правь: мой_текст"
+  - Translate text into English or Russian. Write: "translate: my_text"  or "переведи: мой текст"
+
+The bot doesn't collect any info. Author: @n_log_n
+
+                    '''
+        else:
+            tg.send_message(chat_id, 'I have to think about it. Just a moment ...')
+            input_text = _correct_prompt(input_text)
+            output_text = openai_conn.chat(input_text, chat_temp)
 
     else:
-        info(f"can't parse this type of message: {message}")
-        assert False  # TODO really?
+        error_message = f"Can't parse this type of Telegram message: {message}"
+        info(error_message)
+        output_text = "It seems I can't do what you want. \
+                I can answer to text and transcribe audio into text."
+        output_text += '\n\n' + error_message        
 
     debug(f'{output_text = }')
     debug('finish')
-    return output_text
+    return output_text, chat_id
 
 
 def _init_logging() -> None:
@@ -248,9 +242,8 @@ def telegram_long_polling():
             for result in updates.get('result', []):
                 offset = result.get('update_id', offset)
                 message = result.get('message', {})
-                chat_id = int(message.get('chat', {}).get('id', 0))
-                if chat_id:
-                    result_text = _get_text(message)
+                result_text, chat_id = _get_text_and_chat_id(message)
+                if chat_id != -1:
                     tg.send_message(chat_id, result_text)
         end_time = time.time()
         debug(f'time between requests to Telegram Bot API: {end_time - start_time}')
